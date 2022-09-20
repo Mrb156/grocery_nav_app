c335f2b08f5c4701c109f2c8077e734d402a4670
Mrb156 <morvaibarna@gmail.com>
2022-09-20 19:47:19 +0200

    teszt commit

--- groceryNavApp/lib/screens/home.dart
+++ groceryNavApp/lib/screens/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Lista'),),
+      appBar: AppBar(title: Text('Lista a termékekről'),),
       floatingActionButton: FloatingActionButton.extended(onPressed: (){}, label: Text('Tovább')),
     );
   }
